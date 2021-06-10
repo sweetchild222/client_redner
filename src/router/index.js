00a66c3ba662943e8bc0816a1f84e9e20076adb1
sweetchild222 <sweetchild22.ik@gmail.com>
2021-06-10 12:06:07 +0900

    'ok'

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,17 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
+const pageA = { template: '<div style="color:green;font-size:40px;">client side rendering : page A</div>'}
+const pageB = { template: '<div style="color:blue;font-size:40px;">client side rendering : page B</div>' }
+const pageNotFound = { template: '<div style="color:red;font-size:40px;">page not found</div>' }
 
 export default new VueRouter({
   mode: 'history',
   routes:[
-    { path: '/', redirect: '/foo' },
-    { path: '/foo', component: Foo, name: "foo" },
-    { path: '/bar', component: Bar, name: "bar" }
+    { path: '/', redirect: '/a' },
+    { path: '/a', component: pageA},
+    { path: '/b', component: pageB},
+    { path :'*', component:pageNotFound}
   ]
 })
 
