e21aa790b0bba0581cda6e2fabedff27f0c48420
sweetchild222 <sweetchild22.ik@gmail.com>
2021-06-10 15:55:25 +0900

    'serversize'

---
+++ server_side/a.php
@@ -0,0 +1,3 @@
+<?php
+	echo '<span style="color:black; font-size:40px">server side rendering : page A</span>';
+?>
---
+++ server_side/b.php
@@ -0,0 +1,3 @@
+<?php
+	echo '<span style="color:red; font-size:40px">server side rendering : page B</span>';
+?>
